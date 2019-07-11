5745a19933e23835db3c753e5bbc8c1371d30c91
tongpengfei <821798863@qq.com>
2019-07-11 18:46:21 +0800

    往本地初始化仓库

---
+++ src/main/java/cn/itcast/git/Demo.java
@@ -0,0 +1,12 @@
+package cn.itcast.git;
+
+/**
+ * @author ：tongpengfei
+ * @date ：2019/7/11 17:19
+ * @description： TODO
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
