6ec3d2e044fb299509271dd2bd06f601e6e66c74
tongpengfei <821798863@qq.com>
2019-07-11 18:49:21 +0800

    修改文件

--- src/main/java/cn/itcast/git/Demo.java
+++ src/main/java/cn/itcast/git/Demo.java
@@ -8,5 +8,6 @@ package cn.itcast.git;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2222");
     }
 }
